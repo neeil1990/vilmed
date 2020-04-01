ad5d0b547e1e447118a07b410c87f08306a660e9
neeil1990 <neeil@mail.ru>
2020-04-01 15:55:56 +0300

    no message

--- bitrix/templates/elektro_flat/components/bitrix/news.list/banners_main/template.php
+++ bitrix/templates/elektro_flat/components/bitrix/news.list/banners_main/template.php
@@ -5,26 +5,26 @@ $this->setFrameMode(true);
 if(count($arResult["ITEMS"]) < 1)
 	return;?>
 
-<div class="banners-main">	
+<div class="banners-main">
 	<?$width = 0;
 	foreach($arResult["ITEMS"] as $arItem):
 		if(!isset($arItem["DISPLAY_PROPERTIES"]["WIDTH"]))
 			continue;
 		if($width == 0)
 			echo "<div class='banners-main__row'>";?>
-		<a class="banners-main__item" href="<?=(!empty($arItem['DISPLAY_PROPERTIES']['URL']) ? $arItem['DISPLAY_PROPERTIES']['URL']['VALUE'] : 'javascript:void(0)');?>"<?=(!empty($arItem["DISPLAY_PROPERTIES"]["WIDTH"]) ? " style='width:".$arItem["DISPLAY_PROPERTIES"]["WIDTH"]["VALUE"]."%;'" : "");?>>			
-			<span class="banners-main__item-image"<?=(is_array($arItem["PREVIEW_PICTURE"]) ? " style=\"background-image:url('".$arItem["PREVIEW_PICTURE"]["SRC"]."');\"" : "");?>></span>
+		<a class="banners-main__item" href="<?=(!empty($arItem['DISPLAY_PROPERTIES']['URL']) ? $arItem['DISPLAY_PROPERTIES']['URL']['VALUE'] : 'javascript:void(0)');?>"<?=(!empty($arItem["DISPLAY_PROPERTIES"]["WIDTH"]) ? " style='width:".$arItem["DISPLAY_PROPERTIES"]["WIDTH"]["VALUE"]."%;'" : "");?>>
+			<span class="banners-main__item-image lazy-fadein" data-src="<?=(is_array($arItem["PREVIEW_PICTURE"]) ? $arItem["PREVIEW_PICTURE"]["SRC"] : "");?>"></span>
 			<span class="banners-main__item-block-wrap">
 				<span class="banners-main__item-block">
 					<?if(!empty($arItem["DISPLAY_PROPERTIES"]["BUTTON_TEXT"])):?>
 						<span class="banners-main__item-btn">
-					<?endif;?>					
-					<span class="banners-main__item-text<?=($arItem['DISPLAY_PROPERTIES']['WIDTH']['VALUE'] == '25' ? ' small' : '');?>"><?=$arItem["NAME"]?></span>					
+					<?endif;?>
+					<span class="banners-main__item-text<?=($arItem['DISPLAY_PROPERTIES']['WIDTH']['VALUE'] == '25' ? ' small' : '');?>"><?=$arItem["NAME"]?></span>
 					<?if(!empty($arItem["DISPLAY_PROPERTIES"]["BUTTON_TEXT"])):?>
 						<button name="banners-main__item-button" class="btn_buy"><?=$arItem["DISPLAY_PROPERTIES"]["BUTTON_TEXT"]["VALUE"]?></button>
 						</span>
-					<?endif;?>				
-				</span>				
+					<?endif;?>
+				</span>
 			</span>
 		</a>
 		<?$width += $arItem["DISPLAY_PROPERTIES"]["WIDTH"]["VALUE"];
