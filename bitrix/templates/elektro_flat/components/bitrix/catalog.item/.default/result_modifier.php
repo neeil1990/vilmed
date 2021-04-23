4509f913ced2ff9f0fcaa445c521196c9cd4483d
neeil1990 <neeil@mail.ru>
2021-04-23 16:59:18 +0300

    #116603 article

--- bitrix/templates/elektro_flat/components/bitrix/catalog.item/.default/result_modifier.php
+++ bitrix/templates/elektro_flat/components/bitrix/catalog.item/.default/result_modifier.php
@@ -1,2 +1,3 @@
 <?php
 $arResult['ITEM']['NAME'] = html_entity_decode($arResult['ITEM']['NAME']);
+$arResult['ITEM']["PROPERTIES"]["ARTNUMBER"]["VALUE"] = ($arResult['ITEM']["PROPERTIES"]["ARTNUMBER"]["VALUE"]) ?: $arResult['ITEM']["PROPERTIES"]["CML2_ARTICLE"]["VALUE"];
